ab1ca73cfbcc7cfb42502b474f6983495a4ba956
shyam <shyampathuri3@gmail.com>
2025-12-31 11:14:21 +0530

    feat: enhance validation exception handling and add generic exception response

--- src/main/java/org/codeOn/InterviewHub/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/codeOn/InterviewHub/common/exception/GlobalExceptionHandler.java
@@ -21,8 +21,16 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<?>> handleValidationException(MethodArgumentNotValidException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).
-        body(ApiResponse.failure(ex.getMessage(), "VALIDATION_ERROR"));
+        String message = ex.getBindingResult().getFieldErrors().stream()
+        .map(err-> err.getField()+": "+err.getDefaultMessage())
+        .findFirst()
+        .orElse("Validation error");
+        return ResponseEntity.badRequest().body(ApiResponse.failure(message, "VALIDATION_ERROR"));
     }
-    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+            ApiResponse.failure("Something went wrong", "INTERNAL_SERVER_ERROR")
+        );
+    }
 }
